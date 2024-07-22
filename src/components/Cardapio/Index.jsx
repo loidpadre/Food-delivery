32f92c6bb255a4b7b82893c66b22c850c83f018d
Loid Padre <145937391+loidpadre@users.noreply.github.com>
2024-07-22 21:02:59 +0000

    Get data from api

--- src/components/Cardapio/Index.jsx
+++ src/components/Cardapio/Index.jsx
@@ -1,30 +1,47 @@
-import { Wrapper } from "./style"
-import data from "../../data/data"
-import { Cards } from "../Cards/Cards"
-export const Cardapio = () =>{
-    console.log(data)
-    return(
-        <Wrapper>
-            <div className="container">
-                <div className="title">
-                    <h1>Cardapio</h1>
-                </div>
-                <div className="filter">
-                    <ul>
-                        <li>Refrigerante</li>
-                        <li>Pizza</li>
-                        <li>Hamburgeres</li>
-                        <li>Ver Tudo</li>
-                    </ul>
-                </div>
-                <div className="cards">
-                    {
-                        data.map((items) =>(
-                            <Cards items={items} />
-                        ))
-                    }
-                </div>
-            </div>
-        </Wrapper>
-    )
-}
+import { Wrapper } from "./style";
+import { Cards } from "../Cards/Cards";
+import { useEffect, useState } from "react";
+export const Cardapio = () => {
+  const URL = "https://back-end-delivey-app.onrender.com";
+
+  const [data, setData] = useState([]);
+  const getProd = async () => {
+    try {
+      const response = await fetch(`${URL}/home`);
+      if (!response.ok) {
+        throw new Error("Erro ao buscar produtos");
+      }
+      const data = await response.json();
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Erro ao carregar produtos:", error);
+    }
+  };
+
+  useEffect(() => {
+    getProd();
+  }, []);
+  return (
+    <Wrapper>
+      <div className="container">
+        <div className="title">
+          <h1>Cardapio</h1>
+        </div>
+        <div className="filter">
+          <ul>
+            <li>Refrigerante</li>
+            <li>Pizza</li>
+            <li>Hamburgeres</li>
+            <li>Ver Tudo</li>
+          </ul>
+        </div>
+        <div className="cards">
+          {data.map((items) => (
+            <Cards items={items} />
+          ))}
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
